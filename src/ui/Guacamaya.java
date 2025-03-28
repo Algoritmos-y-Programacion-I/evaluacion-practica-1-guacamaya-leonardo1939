0855ab6fcc669796c44fdc244523d2514bfdf1ea
Leonardo Andres Munoz Sarria <1111672287@u.icesi.edu.co>
2025-03-28 12:13:47 -0500

    Ev Practico 1

--- src/ui/Guacamaya.java
+++ src/ui/Guacamaya.java
@@ -3,45 +3,31 @@ package ui;
 import java.util.Scanner;
 
 public class Guacamaya {
-
-    // Scanner global para todo el programa
-    public static Scanner reader;
-    // Arreglos de precios y unidades para todo el programa
-    public static double[] precios;
-    public static int[] unidades;
-
-    public static void main(String[] args) {
-
-        inicializarGlobales();
-        menu();
-    }
-
-    /**
-     * Descripcion: Este metodo se encarga de iniciar las variables globales
-     * pre: El Scanner reader debe estar declarado
-     * pos: l Scanner reader queda inicializado
-    */
-    public static void inicializarGlobales() {
-
-        reader = new Scanner(System.in);
-
-    }
-
-    /**
-     * Descripcion: Este metodo se encarga de desplegar el menu al usuario y mostrar los mensajes de resultado para cada funcionalidad
-     * pre: El Scanner reader debe estar inicializado
-     * pre: El arreglo precios debe estar inicializado
-    */
-    public static void menu() {
-
-        System.out.println("Bienvenido a Guacamaya!");
-
-        establecerCantidadVendida();
-
-        boolean salir = false;
-
-        do {
-
+        private static double[] precio;
+        private static int[] Cantidades;
+        private static int NumeroProductos;
+        
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            
+            System.out.println("Bienvenido a Guacamaya!");
+            System.out.print("Digite el numero de referencias de producto diferentes vendidas en el dia: ");
+            NumeroProductos = scanner.nextInt();
+            
+            precio = new double[NumeroProductos];
+            Cantidades = new int[NumeroProductos];
+            
+            int option;
+            do {
+                showMenu();
+                option = scanner.nextInt();
+                ManejoOption(option, scanner);
+            } while (option != 6);
+            
+            scanner.close();
+        }
+        
+        private static void showMenu() {
             System.out.println("\nMenu Principal:");
             System.out.println("1. Solicitar precios ($) y cantidades de cada referencia de producto vendida en el dia");
             System.out.println("2. Calcular la cantidad total de unidades vendidas en el dia");
@@ -49,90 +35,76 @@ public class Guacamaya {
             System.out.println("4. Calcular las ventas totales (dinero recaudado) durante el dia");
             System.out.println("5. Consultar el numero de referencias de productos que en el dia han superado un limite minimo de ventas");
             System.out.println("6. Salir");
-            System.out.println("\nDigite la opcion a ejecutar");
-            int opcion = reader.nextInt();
-
-            switch (opcion) {
+            System.out.print("\nDigite la opción a ejecutar: ");
+        }
+        
+        private static void ManejoOption(int option, Scanner scanner) {
+            switch (option) {
                 case 1:
-                    solicitarDatos();
+                    requestProductData(scanner);
                     break;
                 case 2:
-                    System.out.println("\nLa cantidad total de unidades vendidas en el dia fue de: "+calcularTotalUnidadesVendidas());
+                    System.out.println("La cantidad total de unidades vendidas en el dia fue de: " + TotalUnidades());
                     break;
                 case 3:
-                    System.out.println("\nEl precio promedio de las referencias de producto vendidas en el dia fue de: "+calcularPrecioPromedio());
+                    System.out.println("El precio promedio de las referencias de producto vendidas en el dia fue de: " + PrecioPromedio());
                     break;
                 case 4:
-                    System.out.println("\nLas ventas totales (dinero recaudado) durante el dia fueron: "+calcularVentasTotales());
+                    System.out.println("Las ventas totales (dinero recaudado) durante el dia fueron: " + TotalVendidos());
                     break;
                 case 5:
-                    System.out.println("\nDigite el limite minimo de ventas a analizar");
-                    double limite = reader.nextDouble();
-                    System.out.println("\nDe las "+precios.length+" referencias vendidas en el dia, "+consultarReferenciasSobreLimite(limite)+" superaron el limite minimo de ventas de "+limite);
+                    System.out.print("Digite el limite minimo de ventas a analizar: ");
+                    double limit = scanner.nextDouble();
+                    System.out.println("De las " + NumeroProductos + " referencias vendidas en el dia, " + ProductosSobreLimite(limit) + " superaron el limite de ventas de " + limit);
                     break;
                 case 6:
-                    System.out.println("\nGracias por usar nuestros servicios!");
-                    salir = true;
-                    reader.close();
+                    System.out.println("Saliendo del programa...");
                     break;
-
                 default:
-                    System.out.println("\nOpcion invalida, intenta nuevamente.");
-                    break;
+                    System.out.println("Opcion invalida. Intente nuevamente.");
             }
-
-        } while (!salir);
-
-    }
-
-    /**
-     * Descripcion: Este metodo se encarga de preguntar al usuario el numero de referencias de producto diferentes 
-     * vendidas en el dia e inicializa con ese valor los arreglos encargados de almacenar precios y cantidades
-     * pre: El Scanner reader debe estar inicializado
-     * pre: Los arreglos precios y unidades deben estar declarados
-     * pos: Los arreglos precios y unidades quedan inicializados
-     */
-    public static void establecerCantidadVendida() {
-
-        System.out.println("\nDigite el numero de referencias de producto diferentes vendidas en el dia ");
-        int referencias = reader.nextInt();
-
-        precios = new double[referencias];
-        unidades = new int[referencias];
-
-    }
-
-    public static void solicitarDatos(){
-
+        }
         
-     
+        private static void requestProductData(Scanner scanner) {
+            for (int i = 0; i < NumeroProductos; i++) {
+                System.out.print("Ingrese el precio del producto " + (i + 1) + ": ");
+                precio[i] = scanner.nextDouble();
+                System.out.print("Ingrese la cantidad vendida del producto " + (i + 1) + ": ");
+                Cantidades[i] = scanner.nextInt();
+            }
+        }
+        
+        private static int TotalUnidades() {
+            int total = 0;
+            for (int Cantidad : Cantidades) {
+                total += Cantidad;
+            }
+            return total;
+        }
+        
+        private static double PrecioPromedio() {
+            double totalPrecio = 0;
+            for (double price : precio) {
+                totalPrecio += price;
+            }
+            return NumeroProductos > 0 ? totalPrecio / NumeroProductos: 0;
+        }
+        
+        private static double TotalVendidos() {
+            double totalVendido = 0;
+            for (int i = 0; i < NumeroProductos; i++) {
+                totalVendido += precio[i] * Cantidades[i];
+            }
+            return totalVendido;
+        }
+        
+        private static int ProductosSobreLimite(double limit) {
+            int count = 0;
+            for (int i = 0; i < NumeroProductos; i++) {
+                if ((precio[i] * Cantidades[i]) > limit) {
+                    count++;
+                }
+            }
+            return count;
+        }
     }
-
-    public static int calcularTotalUnidadesVendidas(){
-
-        return 0;
-
-
-    }
-
-    public static double calcularPrecioPromedio(){
-
-        return 0;
-
-
-    }
-
-    public static double calcularVentasTotales(){
-
-        return 0;
-
-
-    }
-
-    public static int consultarReferenciasSobreLimite(double limite){
-
-        return 0;
-
-    }
-
-}
